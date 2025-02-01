caccefc4a2d7da7956e8b70d1d23eb6b74a78061
Joseph-A-Mason <mason@geography.wisc.edu>
2025-01-31 20:52:31 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,1 @@
-install.packages(c("aqp", "soilDB", "SoilTaxonomy", "sf", "sharpshootR", "dendextend", "lattice", "maps", "devtools"))
-devtools::install_github("IRkernel/IRkernel")
-IRkernel::installspec()
+install.packages(c("aqp", "soilDB", "SoilTaxonomy", "sf", "sharpshootR", "dendextend", "lattice", "maps"))
